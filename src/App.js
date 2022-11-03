c9542679d1ba1cb87698581c8770fb905ccfce0f
ANUJGARG120 <garganuj470@gmail.com>
2022-11-03 21:27:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import Questions from "./components/Questions";
+import { useState } from "react";
+import Result from "./components/Result";
+import './style.css';
+
+function App() {
+  const [answer, setAnswer] = useState([])
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/questions" element={<Questions answer={answer} setAnswer={setAnswer} />}></Route>
+        <Route path="/solution" element={<Result answer={answer}/>} ></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
